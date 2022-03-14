b5f00485ea296166905e46e99750243cda8559e0
Aric Riggs <ariggs1226@gmail.com>
2022-03-14 15:47:03 -0500

    After Walkthrough with some of my own stuff I was working on during my wait for the walkthrough!

--- app.js
+++ app.js
@@ -1,47 +1,81 @@
-// console.log("whats up") {Test console log}
-//Promise .then() chaining
+// // console.log("whats up") {Test console log}
+// //Promise .then() chaining
 
-slowMath
-    .add(6, 2)
-    .then((res) => {
-        console.log(res);
-        return slowMath.multiply(res, 2)
-    })
-    
-    .then((res) => {
-        console.log(res);
-        return slowMath.divide(res, 4)
-    })
+// slowMath
+//     .add(6, 2)
+//     .then((res) => {
+//         console.log(res);
+//         return slowMath.multiply(res, 2)
+//     })
 
-    .then((res) => {
-        console.log(res);
-        return slowMath.subtract(res, 3)
-    })
+//     .then((res) => {
+//         console.log(res);
+//         return slowMath.divide(res, 4)
+//     })
 
-    .then((res) => {
-        console.log(res);
-        return slowMath.add(res, 98)
-    })
-    .then((res) => {
-        console.log(res);
-        return slowMath.remainder(res, 2)
-    })
-    .then((res) => {
-        console.log(res);
-        return slowMath.multiply(res, 50)
-    })
-    .then((res) => {
-        console.log(res);
-        return slowMath.remainder(res, 40)
-    })
-    .then((res) => {
-        console.log(res);
-        return slowMath.add(res, 32)
-    })
-    .then((res) => {
-        console.log(`The final resulet is ${res}`)
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-    
+//     .then((res) => {
+//         console.log(res);
+//         return slowMath.subtract(res, 3)
+//     })
+
+//     .then((res) => {
+//         console.log(res);
+//         return slowMath.add(res, 98)
+//     })
+//     .then((res) => {
+//         console.log(res);
+//         return slowMath.remainder(res, 2)
+//     })
+//     .then((res) => {
+//         console.log(res);
+//         return slowMath.multiply(res, 50)
+//     })
+//     .then((res) => {
+//         console.log(res);
+//         return slowMath.remainder(res, 40)
+//     })
+//     .then((res) => {
+//         console.log(res);
+//         return slowMath.add(res, 32)
+//     })
+//     .then((res) => {
+//         console.log(`The final resulet is ${res}`)
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// happy pi day!!
+
+//Async / Await
+
+let doMath = async () => {
+    try {
+        let result;
+
+        result = await slowMath.add(6, 2);
+        console.log(result);
+        result = await slowMath.multiply(result, 2);
+        console.log(result);
+        result = await slowMath.divide(result, 4);
+        console.log(result);
+        result = await slowMath.subtract(result, 3);
+        console.log(result);
+        result = await slowMath.add(result, 98);
+        console.log(result);
+        result = await slowMath.remainder(result, 2);
+        console.log(result);
+        result = await slowMath.multiply(result, 50);
+        console.log(result);
+        result = await slowMath.remainder(result, 40);
+        console.log(result);
+        result = await slowMath.add(result, 32);
+        console.log(result);
+        console.log(`The final result is ${result}!`);
+    } catch (err) {
+        console.log(err)
+    } finally {
+        console.clear();
+    }
+};
+
+ doMath();
